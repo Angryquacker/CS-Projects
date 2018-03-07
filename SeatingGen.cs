256da907db850026fa4be38a3b4312a6e90f388f
Travis <tjxhahn@gmail.com>
2018-03-06 21:42:05 -0500

    Create SeatingGen.cs

---
+++ SeatingGen.cs
@@ -0,0 +1,34 @@
+using System;
+
+static class RandomExtensions
+{
+    public static void Shuffle<T> (this Random rng, T[] array)
+    {
+        int n = array.Length;
+        while (n > 1) 
+        {
+            int k = rng.Next(n--);
+            T temp = array[n];
+            array[n] = array[k];
+            array[k] = temp;
+        }
+    }
+    public static void Main(string[] args) {
+        Console.Write("Please enter the number of students in your class: ");
+        try {
+            int numOfStudents = Convert.ToInt32(Console.ReadLine());
+            int[] ids = new int[numOfStudents];
+            for (int i = 0;i < numOfStudents;i++) {
+                ids[i] = i + 1;
+            }
+            new Random().Shuffle(ids);
+            Console.WriteLine("Your order of students is: ");
+            for (int i = 0;i < numOfStudents;i++) {
+                Console.WriteLine(ids[i]);
+            }
+        } catch (FormatException e) {
+            Console.WriteLine("That is not a number you big dummy!");
+            Console.WriteLine("Error = {0}", e);
+        }
+    }
+}
